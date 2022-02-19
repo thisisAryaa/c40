09360187b80b4aa9dc8fe97517dc9023a5e52ce4
Arya <77740630+thisisAryaa@users.noreply.github.com>
2022-02-19 12:47:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
+
+function preload() {
+  backgroundImage = loadImage("background.png");
+  car1_img = loadImage("car1.png");
+  car2_img = loadImage("car2.png");
+  track = loadImage("track.jpg");
+  fuelImage = loadImage("fuel.png");
+  powerCoinImage = loadImage("goldCoin.png");
+  obstacle1Image = loadImage("obstacle1.png");
+  obstacle2Image = loadImage("obstacle2.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
